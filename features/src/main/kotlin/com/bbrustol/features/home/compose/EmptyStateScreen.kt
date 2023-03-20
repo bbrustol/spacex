f075576124f873cf0310e803f70082b349a4aa6e
Bruno Brustoloni <>
2023-03-20 10:19:13 +0000

    feat(empty state): update

--- features/src/main/kotlin/com/bbrustol/features/home/compose/EmptyStateScreen.kt
+++ features/src/main/kotlin/com/bbrustol/features/home/compose/EmptyStateScreen.kt
@@ -1,38 +1,34 @@
 package com.bbrustol.features.home.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bbrustol.uikit.theme.SpacexTheme
 
 @Composable
 fun EmptyStateScreen() {
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = Modifier.fillMaxSize()
+        .background(Color.LightGray.copy(0.3f)),
+        verticalArrangement = Arrangement.Center,
+    ) {
         Image(
-            painter = painterResource(com.bbrustol.uikit.R.drawable.crash_rocket_with_bg),
+            painter = painterResource(com.bbrustol.uikit.R.drawable.empty_state),
             contentDescription = null,
             contentScale = ContentScale.FillWidth,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally).fillMaxWidth()
-            )
-        Column(modifier = Modifier
-            .fillMaxWidth().padding(vertical = 80.dp)
-            ) {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
-                text = "Results not found"
-            )
-        }
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(),
+        )
     }
 }
 
